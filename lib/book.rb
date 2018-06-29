f4d96d46c4c7baf40b0384f2f3946dfa02531466
Michael Morris <mdam2345@gmail.com>
2018-06-29 02:49:01 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -32,9 +32,9 @@ class Book
       @genre
     end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
 end
 
 
