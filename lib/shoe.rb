6f72e719260aa64606d62ec20458a738a858cde0
Michael Morris <mdam2345@gmail.com>
2018-06-29 02:54:23 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,20 @@
 class Shoe
-
+  class Book
+    attr_accessor :brand, :color, :size, :material, :condition
+   
+    def initialize(brand)
+      @brand = brand
+    end
+   
+    def brand
+      @brand
+    end
+   
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+   
+  end
 end
 
 
