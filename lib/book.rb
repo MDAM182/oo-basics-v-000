6776ad7ef13965f97435134fb67fbda4345c2d22
Michael Morris <mdam2345@gmail.com>
2018-06-29 02:48:06 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -17,20 +17,20 @@ class Book
     end
 
     def page_count=(num)
-    @page_count = num
+      @page_count = num
     end
 
     def page_count
-    @page_count
+      @page_count
     end
 
     def genre=(genre)
- @genre = genre
-end
+      @genre = genre
+    end
 
-def genre
- @genre
-end
+    def genre
+      @genre
+    end
 end
 
 
